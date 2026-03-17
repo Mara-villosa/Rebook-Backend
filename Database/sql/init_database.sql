b26b763d1f0e4dcf92d0eeca1cc797ad0d9baa76
Mara <arizagarciamar@gmail.com>
2026-03-17 20:38:59 +0100

    Fixed typo in SQL functions to icreate database

--- Database/sql/init_database.sql
+++ Database/sql/init_database.sql
@@ -4,5 +4,5 @@ CREATE TABLE users (
     id int AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) UNIQUE NOT NULL,
     name varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
 );
