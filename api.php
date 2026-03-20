4555fba2514e0641f0613deba5c0928aa7a1c9f2
Mara <arizagarciamar@gmail.com>
2026-03-20 14:48:31 +0100

    Updated CORS Policy

--- api.php
+++ api.php
@@ -10,6 +10,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Vary: Origin");
 header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
 
 // Handle preflight
