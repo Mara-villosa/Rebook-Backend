814cb74e5c7a64240a2c74282f06103fc79d1139
Mara <arizagarciamar@gmail.com>
2026-04-17 11:47:44 +0200

    feat(favs endpooint): endpoint /fav/get

--- Database/Models/FavouritesModel.php
+++ Database/Models/FavouritesModel.php
@@ -58,12 +58,95 @@ class FavouritesModel{
         return true;
     }
 
+    /**
+     * Elimina un libro de la tabla de favoritos para un usuario
+     * @param int $bookID
+     * @param int $userID
+     * @return bool true si se elimina
+     */
     public function removeBookFromFav(int $bookID, int $userID){
+        $connection = DatabaseConfig::connect();
+        $connection->autocommit(false);
+        $connection->begin_transaction();
 
+        //Quitar el libro de la tabla rented
+        $query = $connection->prepare('DELETE FROM favourites WHERE favourites.id_book = ? AND favourites.id_user = ?');
+        $query->bind_param('ii', $bookID, $userID);
+
+        //Si no se borra ningún libro es porque no estaba añadido como fav, se devuelve false
+        try{
+            $query->execute();
+            if($query->affected_rows === 0){
+                $connection->rollback();
+                $connection->autocommit(true);
+                return false;
+            }
+        }
+        //Se cancela si hay un error
+        catch(Exception $e){
+            $connection->rollback();
+            $connection->autocommit(true);
+            return false;
+        }
+
+        $connection->commit();
+        $connection->autocommit(true);
+        $connection->close();
+
+        return true;
     }
 
-    public function getFavBooks(int $userID){
 
+    /**
+     * Devuelve todos los libros añadidos a la tabla favoritos por un usuario
+     * @param int $userID
+     * @return array|null array de libros si los encuentra, null si no tiene favoritos
+     */
+    public function getFavBooks(int $userID){
+        $connection = DatabaseConfig::connect();
+        $connection->autocommit(false);
+        $connection->begin_transaction();
+
+        $query = $connection->prepare('SELECT * FROM books INNER JOIN favourites ON books.id = favourites.id_book WHERE favourites.id_user = ?');
+        $query->bind_param('i', $userID);
+        $query->execute();
+        $query_result = $query->get_result();
+
+        //Si hay un error o no encuentra el usuario, devuelve null
+        if ($connection->error || $query_result->num_rows === 0) {
+            $query_result->free();
+            $connection->rollback();
+            $connection->autocommit(true);
+            return null;
+        }
+
+        $books = array();
+        while($result = $query_result->fetch_assoc()){
+            $book = new BookDTO(
+                $result['id'],
+                $result['title'],
+                $result['author'],
+                $result['description'],
+                $result['rent_price'],
+                $result['sell_price'],
+                $result['isbn'],
+                $result['url'],
+                $result['category'],
+                $result['in_cart'],
+                $result['rented'], 
+                $result['id_user'],
+                $result['sold'],
+                $result['rent_expiration_date']);
+
+            array_push($books, $book);
+        }
+
+        $query_result->free();
+        $connection->commit();
+        $connection->autocommit(true);
+        $connection->close();
+
+        return $books;
     }
 }
 ?>
--- controllers/favs.controller.php
+++ controllers/favs.controller.php
@@ -28,6 +28,11 @@ require_once(ROOT . '/database/models/FavouritesModel.php');
         exit;
     }
 
+    /**
+     * Endpoint /fav/remove
+     * Elimina un libro de favoritos para el usuario que hace la petición
+     * @param int $userID
+     */
     public static function removeFavBook(int $userID){
         //Se recupera el body de la request en formato JSON
         $inputJSON = file_get_contents('php://input');
@@ -35,10 +40,42 @@ require_once(ROOT . '/database/models/FavouritesModel.php');
 
         //Bad Request si faltan campos
         if(!isset($data['book_id'])) returnHTTPError('Book ID not provided', 400);
+
+        $model = new FavouritesModel();
+        $removed = $model->removeBookFromFav($data['book_id'], $userID);
+
+        if(!$removed) returnHTTPError('Fav removal failed', 400);
+        
+        http_response_code(200);
+        header('Content-Type: application/json');
+
+        $response = array('message' => 'book removed from favourites');
+
+        echo json_encode($response);
+        exit;
     }
 
+    /**
+     * Endpoint /fav/get
+     * Recupera un array con todos los favoritos de un usuario
+     * @param int $userID
+     */
     public static function getFavBooks(int $userID){
+        $model = new FavouritesModel();
+        $books = $model->getFavBooks($userID);
 
+        if(!isset($books)) returnHTTPError('Fav Books not found for this user', 404);
+
+        http_response_code(200);
+        header('Content-Type: application/json');
+
+        $response = array("favourites" => []);
+        for($i = 0; $i < count($books); $i++){
+            array_push($response['favourites'], $books[$i]->jsonSerialize());
+        } 
+
+        echo json_encode($response);
+        exit;
     }
  }
 ?>
